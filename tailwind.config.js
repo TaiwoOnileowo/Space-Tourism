f8bdcf1ff8e6d5a0dcdba611334b3f9c200c11f6
Onileowo Taiwo <taiwoonileowo17@gmail.com>
2024-05-01 13:48:37 +0100

    Delete ReadMe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [".index.html", ".src***.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        homeDesktop: "url('src/HOMEPAGE/assets/bghomedesktop.jpg')",
-        homeTablet: "url('src/HOMEPAGE/assets/bghometablet.jpg')",
-        homeMobile: "url('src/HOMEPAGE/assets/bghomemobile.jpg')",
-        destinationDesktop:
-          "url('src/DESTINATION/assets/bgdestinationdesktop.jpg')",
-        destinationTablet:
-          "url('src/DESTINATION/assets/bgdestinationtablet.jpg')",
-        destinationMobile:
-          "url('src/DESTINATION/assets/bgdestinationmobile.jpg')",
-        crewDesktop: "url('src/CREW/assets/bgcrewdesktop.jpg')",
-        crewTablet: "url('src/CREW/assets/bgcrewtablet.jpg')",
-        crewMobile: "url('src/CREW/assets/bgcrewmobile.jpg')",
-        technologyDesktop: "url('src/TECHNOLOGY/assets/bgtechnologydesktop.jpg')",
-        technologyTablet: "url('src/TECHNOLOGY/assets/bgtechnologytablet.jpg')",
-        technologyMobile: "url('src/TECHNOLOGY/assets/bgtechnologymobile.jpg')",
+        homeDesktop: 'url("bghomedesktop.jpg")',
+        homeTablet: "url('bghometablet.jpg')",
+        homeMobile: "url('bghomemobile.jpg')",
+        destinationDesktop: "url('bgdestinationdesktop.jpg')",
+        destinationTablet: "url('bgdestinationtablet.jpg')",
+        destinationMobile: "url('bgdestinationmobile.jpg')",
+        crewDesktop: "url('bgcrewdesktop.jpg')",
+        crewTablet: "url('bgcrewtablet.jpg')",
+        crewMobile: "url('bgcrewmobile.jpg')",
+        technologyDesktop: "url('bgtechnologydesktop.jpg')",
+        technologyTablet: "url('bgtechnologytablet.jpg')",
+        technologyMobile: "url('bgtechnologymobile.jpg')",
       },
     },
     blur: {
-      81.55: "81.55px", // Add your custom blur value
+      81.55: "81.55px",
     },
     screens: {
       ip: "200px",
